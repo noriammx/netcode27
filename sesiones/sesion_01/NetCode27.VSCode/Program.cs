031e6dbc8fe8bec0c85dcff0a50814f2d5d9ae52
Marino Martínez <marino.martinez@outlook.com>
2020-06-06 13:37:39 -0500

    Proyecto de ejemplo con vs code

---
+++ sesiones/sesion_01/NetCode27.VSCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCode27.VSCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
