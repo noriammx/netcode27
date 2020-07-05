61be0442f9e1b5b322be222dc4289eebda3546c6
Marino Martínez <marino.martinez@outlook.com>
2020-07-05 17:23:17 -0500

    Implementación de Entity Framework Core en el proyecto de WEb API, esto para la carpeta sesion_04.

---
+++ sesiones/sesion_04/WebAPINetCode27/ModelDB/Personas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAPINetCode27.ModelDB
+{
+    public partial class Personas
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string ApPaterno { get; set; }
+        public string ApMaterno { get; set; }
+        public int Edad { get; set; }
+    }
+}
