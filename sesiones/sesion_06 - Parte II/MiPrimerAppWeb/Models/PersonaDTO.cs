0b78fa317453a78955777fb50282252b32fc7310
Marino Martínez <marino.martinez@outlook.com>
2020-08-03 22:49:09 -0500

    Implementación de las funciones para eliminar y actualizar registros con la aplicación Web ASP .Net Core MVC

---
+++ sesiones/sesion_06 - Parte II/MiPrimerAppWeb/Models/PersonaDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiPrimerAppWeb.Models
+{
+    public class PersonaDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string ApPaterno { get; set; }
+        public string ApMaterno { get; set; }
+        public int Edad { get; set; }
+    }
+}
---
+++ sesiones/sesion_06 - Parte II/WebAPINetCode27/CorsAuthorizationFilterFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebAPINetCode27
+{
+    internal class CorsAuthorizationFilterFactory : IFilterMetadata
+    {
+        private string v;
+
+        public CorsAuthorizationFilterFactory(string v)
+        {
+            this.v = v;
+        }
+    }
+}
